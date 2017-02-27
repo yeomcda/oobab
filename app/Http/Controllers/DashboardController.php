7328ebdd9c19a0cc8766eab7677223e3d490f4a2
yeomcda <yeomcda@daum.net>
2017-02-27 13:39:21 +0900

    [v.0.1.0] 대시보에드 미입금내역 안내표

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function getIndex()
     {
-        return view('dashboard.index');
+        $notCheckoutOrders = Auth::user()
+            ->orders()
+            ->select('id')
+            ->where('make_checkout_id', '<>', 0)
+            ->where('checkout_id', '=', 0)
+            ->first();
+
+        return view('dashboard.index', ['notCheckoutOrders'=>$notCheckoutOrders]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,4 +6,13 @@
 
 @section('content')
     <h1>DashBoard</h1>
+    @if(!is_null($notCheckoutOrders))
+        <a href="{{ route('checkout.index') }}">
+            <div class="alert alert-danger" role="alert">
+                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                <span class="sr-only">Error:</span>
+                미입금된 정산내역이 존재합니다. 클릭하여 확인해 주세요 :-ㅇ
+            </div>
+        </a>
+    @endif
 @endsection
